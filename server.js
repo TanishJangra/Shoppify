343f4595e4f1ceb70ad540ebd5f90726b82cf865
TanishJangra <96981050+TanishJangra@users.noreply.github.com>
2023-10-18 19:00:29 +0530

    Updated

--- server.js
+++ server.js
@@ -8,11 +8,15 @@ import cors from 'cors'
 import categoryRoutes from './routes/categoryRoutes.js'
 import productRoutes from './routes/productRoutes.js'
 import path from 'path'
+import {fileURLToPath} from 'url';
 dotenv.config();
 
 // database config
 connectDB();
 
+// esmodule fix
+const __filename=fileURLToPath(import.meta.url);
+const __dirname=path.dirname(__filename);
 const app=express()
 
 // middleware
